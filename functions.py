bf719b7a6434e7f078e3867daa7b9de6ffeada84
Ahmed walid abd elwahab <163213850+Ahmedwalid215@users.noreply.github.com>
2024-03-18 17:46:02 +0200

    add new files

---
+++ functions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 02:47:49 2024
+
+@author: DELL
+"""
+
+def add_func(x,y):
+    return x+y
+
+def sub_func(x,y):
+    return x-y
+
+def mult_func(x,y):
+    return x * y
+
+def div_func(x,y):
+    return x / y
